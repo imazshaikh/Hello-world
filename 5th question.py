4686160a080f822080c176b25d9175597df71b86
imazshaikh <73476768+imazshaikh@users.noreply.github.com>
2021-01-06 14:23:46 +0530

    Assignment

---
+++ 5th question.py
@@ -0,0 +1,11 @@
+
+imaz.txt = input("Enter file name: ")
+
+num_words = 0
+
+with open(imaz.txt, 'r') as f:
+    for line in f:
+        words = line.split()
+        num_words += len(words)
+print("Number of words:")
+print(num_words)
